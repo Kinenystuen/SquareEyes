a7654cc1773d55fa67875fcc97f8420f7aa93a89
Kinenystuen <kine_on@hotmail.com>
2023-06-12 19:14:14 +0200

    about us and contavt us

--- js/batman_moviepage.js
+++ js/batman_moviepage.js
@@ -1,13 +1,22 @@
-const trailerplayer = document.getElementById("trailerplayer");
-    trailerplayer.style.display ="none";
-    
-    function watchtrailer() {                            
-    if (playbutton.textContent === "Watch trailer") {
-        playbutton.textContent = "Close trailer";
-        trailerplayer.style.display = "block";
-    } 
-    else {
-        playbutton.textContent = "Watch trailer";
-        trailerplayer.style.display = "none";
+    trailerplayer.style.display = "none";
+    close_trailer.style.display = "none";
+    function watchtrailer() {
+        const trailerplayer = document.getElementById("trailerplayer");
+        const movietrailer = document.getElementById("movietrailer");
+        const close_trailer = document.getElementById("close_trailer");
+        const batmansection1 = document.getElementById("batmansection1");
+        if (playbutton.textContent === "Watch trailer") {
+            playbutton.textContent = "Close trailer";
+            trailerplayer.style.display = "block";
+            close_trailer.style.display = "flex";
+            movietrailer.scrollIntoView({ 
+                behavior: 'smooth' });
+        }
+        else {
+            playbutton.textContent = "Watch trailer";
+            trailerplayer.style.display = "none";
+            close_trailer.style.display = "none";
+            batmansection1.scrollIntoView({ 
+                behavior: 'smooth' });
+        }
     }
-}
